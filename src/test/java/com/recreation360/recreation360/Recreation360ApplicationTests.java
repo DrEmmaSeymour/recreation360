24eed9ed5e99dcef3e7a95f5c594a312d5ec9c7a
Dr. Emma Seymour <emmaseymour2027@gmail.com>
2025-10-11 17:14:17 -0400

    Initial commit - Recreation360 setup

---
+++ src/test/java/com/recreation360/recreation360/Recreation360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recreation360.recreation360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recreation360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
