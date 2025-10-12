1a552875fdb041be5303334b04a72932762f5b8f
Dr. Emma Seymour <emmaseymour2027@gmail.com>
2025-10-11 21:07:39 -0400

    Initial working Spring Boot app for Recreation360

---
+++ src/main/java/com/recreation360/Recreation360Application.java
@@ -0,0 +1,13 @@
+package com.recreation360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recreation360Application {
+    public static void main(String[] args) {
+       SpringApplication.run(Recreation360Application.class, args);
+       System.out.println("Recreation360 Application");
+        
+    }
+}
